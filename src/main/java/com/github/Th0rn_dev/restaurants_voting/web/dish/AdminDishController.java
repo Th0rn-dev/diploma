467a10b6cc0b35e173484037966b38faf58c3ddd
Th0rn-dev <solnek33@gmail.com>
2023-01-22 12:05:34 +0300

    Edited dish entity
    Added field date and anique constrains for fields: date, name, menu.
    Added two methods in AdminDishController:
      getAll() - get all dishes with sorted by date
      get() - get dish by id
    
    Added in README.md two request for view all dishes and dish by id.
    Deleted method prepareAndSave()

--- src/main/java/com/github/Th0rn_dev/restaurants_voting/web/dish/AdminDishController.java
+++ src/main/java/com/github/Th0rn_dev/restaurants_voting/web/dish/AdminDishController.java
@@ -5,7 +5,10 @@ import com.github.Th0rn_dev.restaurants_voting.util.validation.ValidationUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +19,7 @@ import com.github.Th0rn_dev.restaurants_voting.model.Dish;
 import com.github.Th0rn_dev.restaurants_voting.repository.DishRepository;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = AdminDishController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -30,10 +34,17 @@ public class AdminDishController {
         this.repository = repository;
     }
 
-    protected Dish prepareAndSave(Dish dish) {
-        return repository.save(dish);
+    @GetMapping
+    public List<Dish> getAll(){
+        log.info("getAll dish");
+        return repository.findAll(Sort.by(Sort.Direction.DESC,"date"));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Dish> get(@PathVariable int id) {
+        log.info("get dish with {}", id);
+        return ResponseEntity.of(repository.findById(id));
+    }
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable int id){
@@ -46,6 +57,6 @@ public class AdminDishController {
     public void update(@Valid @RequestBody Dish dish, @PathVariable int id) {
         log.info("update {} with  id={}", dish, id);
         ValidationUtil.assureIdConsistent(dish, id);
-        prepareAndSave(dish);
+        repository.save(dish);
     }
 }
